10250f3189b0f84b2b76575d75e0ff492eeb718e
Anthony Sneed <tony@tonysneed.com>
2017-03-07 08:39:59 +0000

    Updated packages, gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var tslint = require('gulp-tslint');
 var del = require('del');
-var exec = require('child_process').execSync;
+var exec = require('child_process').exec;
 
 gulp.task('vet', function() {
     return gulp.src(['./src/**/*.ts', 'src/**/*.spec.ts'])
@@ -9,8 +9,11 @@ gulp.task('vet', function() {
     .pipe(tslint.report());
 });
 
-gulp.task('clean', function() {
-    return del('./dist/');
+gulp.task('compile', ['vet', 'clean'], function(done) {
+    exec('tsc -p src', function (err, stdout, stderr) {
+        console.log(stdout);
+        done();
+    });
 });
 
 gulp.task('compile', ['vet', 'clean'], function() {
